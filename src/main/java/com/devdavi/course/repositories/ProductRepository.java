af5bac88bdeb9253ec947fb1aac5c73a4ee4f00a
Davi <davi.lima@ucsal.edu.br>
2021-04-13 16:14:51 -0300

    Product

---
+++ src/main/java/com/devdavi/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.devdavi.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devdavi.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+}
